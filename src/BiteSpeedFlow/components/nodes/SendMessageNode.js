8e5b8df3ba8c9444db47112b8d03ff9bc65ee115
Sushobhana Patra <sushobhana.patra@rallyhealth.com>
2022-10-28 12:48:56 +0530

    added css

--- src/BiteSpeedFlow/components/nodes/SendMessageNode.js
+++ src/BiteSpeedFlow/components/nodes/SendMessageNode.js
@@ -1,26 +1,39 @@
 import { useCallback, useState } from 'react';
 import { Handle, Position } from 'reactflow';
 import useStore from '../../store';
+import './SendMessageNode.css';
 
 const handleStyle = { left: 10 };
 
 export function SendMessageNode({ id, data }) {
-  const onChange = useCallback((evt) => {
-    console.log(evt.target.value);
-  }, []);
-  const nodes = useStore((s) => s.nodes);
-  const getIsSourceOccupied = (nodeId) => nodes.find((node) => node.id === nodeId)?.data.isSourceOccupied;
-  const isValidConnection = (connection) => {console.log(getIsSourceOccupied(connection.source)); return !getIsSourceOccupied(connection.source);}
+    const onChange = useCallback((evt) => {
+        console.log(evt.target.value);
+    }, []);
+    const nodes = useStore((s) => s.nodes);
+    const getIsSourceOccupied = (nodeId) => nodes.find((node) => node.id === nodeId)?.data.isSourceOccupied;
+    const isValidConnection = (connection) => { console.log(getIsSourceOccupied(connection.source)); return !getIsSourceOccupied(connection.source); }
 
-  return (
-    <>
-      <Handle type="target" position={Position.Left} />
-      <div>
-        <label >Send Message</label>
-        <br></br>
-        <label>{data.message}</label>
-      </div>
-      <Handle type="source" position={Position.Right} id="a" isValidConnection={isValidConnection}/>
-    </>
-  );
+    return (
+        <>
+            <Handle type="target" position={Position.Left} />
+            <div className="custom-node">
+                <div className="custom-node-head">
+                    {data.bIcon && (
+                        <i className={`bi ${data.bIcon} custom-node-head-icon`}></i>
+                    )}
+                    <p className="custom-node-head-title">Send Message</p>
+                </div>
+                <div className="custom-node-body">
+                    <p
+                        style={data.style}
+                        className="custom-node-body-text"
+                    >
+                        {data.message}
+                    </p>
+
+                </div>
+            </div>
+            <Handle type="source" position={Position.Right} id="a" isValidConnection={isValidConnection} />
+        </>
+    );
 }
